511bf91abaada6b81233a23f6f65c7ec4dd3b430
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 00:26:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < ranks.length; i++) {
+    let old_number = parseInt(ranks[i].innerHTML, 10);
+    ranks[i].innerHTML = old_number + n;
+  }
+}
+
+function deepestChild() {
+  let grand_nodes = document.querySelectorAll('div#grand-node div');
+  return grand_nodes[grand_nodes.length -1];
+}
