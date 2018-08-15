960b8851b92a8b029b25e8e09d2c669b5cfcfe26
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 00:08:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('div#nested div.target');
 }
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li').innerhtml
+  for (i = 0; i < ranks.length; i++) {
+    parseInt(ranks[i].innerHTML, 10) + 1;
+  }
+}
