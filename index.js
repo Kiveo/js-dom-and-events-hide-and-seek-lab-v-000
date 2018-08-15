4e89d3acc79e3d2a6babf521bb9a50c572f1465f
Maverick Tesini <mavericktesini@gmail.com>
2018-08-15 00:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function increaseRankBy(n) {
   let ranks = document.querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < ranks.length; i++) {
     console.log(ranks[i]);
+    console.log("HELLO!?!?!??!");
     parseInt(ranks[i], 10) + n;
   }
 }
